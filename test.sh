7245abf8f71f0d1d86d34899073ca584ebac4ea1
huan <huan@huandeMacBook-Pro.local>
2020-03-07 21:52:45 +0800

    First commit Message

---
+++ test.sh
@@ -0,0 +1,7 @@
+SECONDS=0
+# do some work
+python TagSeek.github.py -f demo -t 'CCUGAACCUGAACCUGAACCUGAACCUGAACCUGAACCUGAACCUGAACCUGAACCUGAACCUGAA' -s 12
+#python TagSeqQuant.github.py demo TAIR10.trans.fa TAIR10.genome.fa
+
+duration=$SECONDS
+echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
