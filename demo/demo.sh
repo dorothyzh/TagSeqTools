a14a208653a076cf333575bae36313d463691b55
Huan ZHONG <14485745@life.hkbu.edu.hk>
2020-03-18 15:06:36 +0800

    Update demo.sh

--- demo/demo.sh
+++ demo/demo.sh
@@ -3,8 +3,8 @@ SECONDS=0
 tar -zxvf TAIR10.trans.fa.tar.gz 
 tar -zxvf TAIR10.genome.fa.tar.gz
 
-python TagSeek.NPdemo.py --fastq demo --tag 'CCUGAACCUGAACCUGAACCUGAACCUGAACCUGAACCUGAACCUGAACCUGAACCUGAACCUGAA' --similarity 12
-python TagSeqQuant.NPdemo.py --name demo --trans TAIR10.trans.fa --genome TAIR10.genome.fa
+python TagSeek.py --fastq demo --tag 'CCUGAACCUGAACCUGAACCUGAACCUGAACCUGAACCUGAACCUGAACCUGAACCUGAACCUGAA' --similarity 12
+python TagSeqQuant.py --name demo --trans TAIR10.trans.fa --genome TAIR10.genome.fa
 
 duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
